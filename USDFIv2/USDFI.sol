7bfcb84f7b1f71f01a5237fee62ca21ea1bf29b0
AureumVictoria <54036863+AureumVictoria@users.noreply.github.com>
2023-11-18 19:31:44 +0100

    Update USDFI.sol

--- USDFIv2/USDFI.sol
+++ USDFIv2/USDFI.sol
@@ -1,4 +1,13 @@
-// SPDX-License-Identifier: MIT
+/**
+ * @title STABLE MINTER
+ * @dev STABLE_MINTER contract
+ *
+ * @author - <USDFI TEAM>
+ *
+ * SPDX-License-Identifier: MIT
+ *
+ **/
+
 pragma solidity ^0.8.23;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
